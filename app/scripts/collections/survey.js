df874394f228fb4fc45af0aef2918429782f3d29
Steve Kinney <hello@stevekinney.net>
2014-03-04 17:53:40 -0500

    Surveys are made of respondents

--- app/scripts/collections/survey.js
+++ app/scripts/collections/survey.js
@@ -3,12 +3,12 @@
 define([
     'underscore',
     'backbone',
-    'models/survey'
+    'models/respondent'
 ], function (_, Backbone, SurveyModel) {
     'use strict';
 
     var SurveyCollection = Backbone.Collection.extend({
-        model: SurveyModel
+        model: RespondentModel
     });
 
     return SurveyCollection;
