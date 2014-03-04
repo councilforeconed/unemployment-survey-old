2d90f8c1ecd66c9b52e11ecce63a0192708aace1
Steve Kinney <hello@stevekinney.net>
2014-03-04 17:51:32 -0500

    Change instances of model to collection

--- app/scripts/views/survey.js
+++ app/scripts/views/survey.js
@@ -20,11 +20,11 @@ define([
         events: {},
 
         initialize: function () {
-            this.listenTo(this.model, 'change', this.render);
+            this.listenTo(this.collection, 'change', this.render);
         },
 
         render: function () {
-            this.$el.html(this.template(this.model.toJSON()));
+            this.$el.html(this.template(this.collection.toJSON()));
         }
     });
 
